e1978d1a3ca24652b9cb6f354071614370707cd7
Cassandra Chin <cassandra.chin@gmail.com>
2026-06-12 10:48:12 -0700

    Bump version to 0.2.1

--- src/cli.ts
+++ src/cli.ts
@@ -11,7 +11,7 @@ import { runSkillInstall, runSkillStatus, runSkillUninstall } from './commands/s
 import { runHook, type HookEvent } from './commands/hook.ts';
 import { eventTypeList } from './core/schema.ts';
 
-const VERSION = '0.2.0';
+const VERSION = '0.2.1';
 
 /** Wrap a command action so errors print a clean message and set exit code 1. */
 function action<A extends unknown[]>(fn: (...args: A) => Promise<unknown>) {
